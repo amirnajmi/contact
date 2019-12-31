13cbadb966858fcde837530c03450a795b04b375
amnj <am.nj@outlook.com>
2020-01-01 02:49:40 +0330

    finishing

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Getting Started
+
+Snappbox assignment project needs docker and docker-compose for starting.
+
+After cloning the project, You must run these two commands to create docker image and running docker compose, to prepare
+other containers.
+
+1. > docker build . -t snappbox/contact:1.0
+2. > docker-compose up
+
+* The project will be available on port 8585, and the base path is:
+> */snappbox/resources*
+* All the APIs supports *Hateoas*
+> http://localhost:8585/snappbox/resources/contact
+# Test APIs
+For testing apis there is swagger-ui that will be available throw this address: 
+> http://localhost:8585/swagger-ui.html
+
--- src/main/java/com/snappbox/contact/ContactApplication.java
+++ src/main/java/com/snappbox/contact/ContactApplication.java
@@ -4,10 +4,12 @@ package com.snappbox.contact;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class})
+@EnableMongoRepositories(basePackages = "com.snappbox.contact.repository")
 public class ContactApplication {
 
     public static void main(String[] args) {
