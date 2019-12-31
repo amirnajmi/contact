da3d854e21941326cf797eae6769c3f4920bd2d8
AmirHossein <a.najmi@kian.digital>
2019-12-31 17:32:48 +0330

    test

--- src/test/java/it/ContactIT.java
+++ src/test/java/it/ContactIT.java
@@ -2,54 +2,58 @@ package it;
 
 import com.mongodb.client.MongoClients;
 import com.snappbox.contact.ContactApplication;
+import com.snappbox.contact.DataSetUtil;
 import io.restassured.RestAssured;
-import jdk.nashorn.internal.objects.Global;
-import jdk.nashorn.internal.parser.JSONParser;
 import org.json.JSONObject;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
-
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+
+@RunWith(SpringRunner.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@Import(EmbeddedMongoAutoConfiguration.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = ContactApplication.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {ContactApplication.class, DataSetUtil.class})
 public class ContactIT {
 
     @LocalServerPort
     String port;
-@Autowired
+    @Autowired
     MongoOperations mongoOperation;
+    @Autowired
+    DataSetUtil dataSetUtil;
     String createData;
 
-    @BeforeAll
+    @Before
     public void init() {
         RestAssured.port = Integer.valueOf(port);
-        RestAssured.baseURI = "localhost:8585/snapbox/resources";
-        this.mongoOperation= new MongoTemplate(MongoClients.create(), "test");
-        String path = this.getClass().getClassLoader().getResource("dataset/CreateContact.json").getPath();
-
-        JSONObject jsonObject= (JSONObject) new JSONParser(path, Global.instance(),false).parse();
+        RestAssured.basePath = "/snapbox/resources";
+        this.mongoOperation = new MongoTemplate(MongoClients.create(), "test");
+        createData = dataSetUtil.getJsonSample("CreateContact.json");
 
     }
 
     @Test
-    public void create() {
-        given().basePath("/contact").header("Content-Type","application/json").body(createData).post().then().statusCode(201);
+    public void testCreateContactShouldReturnCreatedStatus() {
+        given().basePath("/contact").header("Content-Type", "application/json").body(createData).put().then().statusCode(201);
+    }
+
+    @Test
+    public void testFilterContactShouldReturnLastCreatedContact(){
+        final JSONObject jsonObject = new JSONObject(createData);
+        given().basePath("/contact/search")
+                .queryParam("name", jsonObject.get("name"))
+                .get()
+                .then().statusCode(200).body(containsString("name")).body(containsString("email")).body(containsString("organization"))
+                .body(containsString("phoneNumber")).body(containsString("github")).body(containsString("repository"));
     }
 
 }
